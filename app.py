3d1b2535f639e89f85a6aba0e793096659debd27
Devanshvarma8891 <devanshvarma91@gmail.com>
2025-05-03 14:40:27 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+import time
+
+app = Flask(__name__)
+
+def slow_text(text, delay=0.1):
+    # This function can be used for visual effects if needed later
+    return ''.join(char for char in text)
+
+@app.route("/", methods=["GET", "POST"])
+def birthday():
+    if request.method == "POST":
+        name = request.form["name"]
+        return render_template("surprise.html", name=name)
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
